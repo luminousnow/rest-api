76a6273fae664edb4d022b291d2818bad3667404
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-11 01:45:25 +0200

    add UPDATE avatar function

--- model/users.js
+++ model/users.js
@@ -16,8 +16,8 @@ const findByToken = async token => {
 }
 
 // === CREATE user ===
-const create = async ({ name, email, password, subscription }) => {
-  const user = new User({ name, email, password, subscription })
+const create = async ({ name, email, password, avatarURL, subscription }) => {
+  const user = new User({ name, email, password, avatarURL, subscription })
 
   return await user.save()
 }
@@ -27,10 +27,16 @@ const updateToken = async (id, token) => {
   return await User.updateOne({ _id: id }, { token })
 }
 
+// === UPDATE avatar  ===
+const updateAvatar = async (id, avatar) => {
+  return await User.updateOne({ _id: id }, { avatar })
+}
+
 module.exports = {
   findByEmail,
   findById,
   findByToken,
   create,
   updateToken,
+  updateAvatar,
 }
