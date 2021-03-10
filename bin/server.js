056bfb2c5c8336fd2ef38a1126371a4f0293acd1
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-10 17:14:41 +0200

    add create-dir code

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,18 @@
 const app = require('../app')
 const db = require('../model/db')
+const createFolder = require('../helpers/create-dir')
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    const UPLOAD_DIR = process.env.UPLOAD_DIR
+    const AVATAR_OF_USER = process.env.AVATAR_OF_USER
+
+    await createFolder(UPLOAD_DIR)
+    await createFolder(AVATAR_OF_USER)
+
     console.log(`Server running. Use our API on port ${PORT}`)
   })
 }).catch(error => {
