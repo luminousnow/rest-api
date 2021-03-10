3e344cc283e8e35d44b1f15713570f2cfafa60ba
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-03-11 01:43:38 +0200

    add router for avatar

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -18,6 +18,11 @@ router.post('/auth/logout', guard, userController.logOut)
 router.get('/current', guard, userController.getUser)
 
 // === router PATCH ===
-router.get('/avatars', guard, upload.single('avatar'), userController.setAvatar)
+router.patch(
+  '/avatars',
+  guard,
+  upload.single('avatar'),
+  userController.setAvatar,
+)
 
 module.exports = router
